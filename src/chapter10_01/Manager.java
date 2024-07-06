2d6e04025bfe606faf78a9bb049262c002ded373
MeiKawanishi <xoxo.nyanchuuu@gmail.com>
2024-07-06 20:47:07 +0900

    update: chapter10_01

--- src/chapter10_01/Manager.java
+++ src/chapter10_01/Manager.java
@@ -1,6 +1,10 @@
 package chapter10_01;
 
-class Manager extends Employee{
+public class Manager extends Employee{
+	
+	public Manager() {
+		super();
+	}
 
 	public void management() {
 		System.out.println(name + "は管理業務を行ないます。");
