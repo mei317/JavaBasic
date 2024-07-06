424ec55b1541feec52e5dce7ea8d2081ec139849
MeiKawanishi <xoxo.nyanchuuu@gmail.com>
2024-07-06 20:46:35 +0900

    update: chapter09_01

--- src/chapter09_01/Item.java
+++ src/chapter09_01/Item.java
@@ -2,17 +2,14 @@ package chapter09_01;
 
 public class Item {
 
-//	メンバ変数
 	private String name;
 	private int price;
 	
-//	コンストラクタ
 	public Item (String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
 	
-//	メソッド
 	public void display () {
 		System.out.print(this.name + ":");
 		System.out.println(this.price + "円");
