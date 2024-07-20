15b2b0864f07a35b83195b1b2c99e378634f1208
MeiKawanishi <xoxo.nyanchuuu@gmail.com>
2024-07-20 21:39:22 +0900

    chapter14_01

---
+++ src/chapter14_01/Define.java
@@ -0,0 +1,7 @@
+package chapter14_01;
+
+public class Define {
+	
+	public static final String number = "定義クラス";
+	
+}
---
+++ src/chapter14_01/Main.java
@@ -0,0 +1,13 @@
+package chapter14_01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String x = Define.number;
+		
+		System.out.println(x);
+	}
+
+}
